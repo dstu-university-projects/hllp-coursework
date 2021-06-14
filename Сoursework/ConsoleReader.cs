e3cbd61e93e4f6ee30956dfb463febaee0ea4f26
alexey.nikolaenko <nashron.developer@gmail.com>
2021-06-14 20:15:53 +0300

    Сделал helper для чтения из консоли

---
+++ Сoursework/ConsoleReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Сoursework
+{
+    public class ConsoleReader
+    {
+        public static decimal getNumber(string label)
+        {
+            decimal input;
+            bool isReading;
+            do
+            {
+                isReading = !decimal.TryParse(RequestValue(label), out input);
+                if (isReading)
+                {
+                    Console.WriteLine("I can't read that value! Please try again...");
+                }
+            } while (isReading);
+            return input;
+        }
+
+        public static string RequestValue(string label)
+        {
+            Console.WriteLine(label);
+            return Console.ReadLine();
+        }
+    }
+}
