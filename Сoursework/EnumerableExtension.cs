2508d7b0f91627665e1808ae648e5cc9129477ef
alexey.nikolaenko <nashron.developer@gmail.com>
2021-06-16 19:24:08 +0300

    Сделал расширения для колекций

---
+++ Сoursework/EnumerableExtension.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Collections.Generic;
+
+namespace Сoursework
+{
+    public static class EnumerableExtension
+    {
+        public static string ToPresentationString<T>(this IEnumerable<T> enumerable)
+        {
+            return "[" + string.Join(", ", enumerable) + "]";
+        }
+    }
+}
---
+++ Сoursework/ListExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Сoursework
+{
+    public static class ListExtension
+    {
+        public static int FirstIndexWhere<T>(this IList<T> list, Predicate<T> predicate)
+        {
+            for (int index = 0; index < list.Count; index++)
+            {
+                if (predicate(list[index]))
+                {
+                    return index;
+                }
+            }
+            return -1;
+        }
+
+        public static int LastIndexWhere<T>(this IList<T> list, Predicate<T> predicate)
+        {
+            return list.Count - FirstIndexWhere(list.Reverse().ToArray(), predicate) - 1;
+        }
+    }
+}
